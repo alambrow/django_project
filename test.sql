136733bf9502db751a07ccb7598d56b795a0d5ba
alambrow <alexander.lambrow@gmail.com>
2021-08-03 11:00:30 -0500

    ch 5 complete

--- test.sql
+++ test.sql
@@ -2,3 +2,16 @@ SELECT * FROM auth_user;
 SELECT * FROM authtoken_token;
 SELECT * FROM levelupapi_gamer;
 SELECT * FROM levelupapi_game;
+SELECT * FROM levelupapi_event;
+
+SELECT * FROM levelupapi_game;
+
+SELECT 
+    g.id,
+    g.name,
+    g.description,
+    g.number_of_players,
+    g.maker,
+    g.game_type_id,
+FROM levelupapi_game g
+WHERE g.id = 1;
